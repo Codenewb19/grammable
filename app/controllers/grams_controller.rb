f178da45605ce31d838c9f3c23c49a9287d3c546
Evan Goldman <pitlere@yahoo.com>
2019-11-04 19:52:23 +0000

    lesson 14

--- app/controllers/grams_controller.rb
+++ app/controllers/grams_controller.rb
@@ -1,6 +1,13 @@
 class GramsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create]
 
+  def destroy
+    @gram = Gram.find_by_id(params[:id])
+    return render_not_found if @gram.blank?
+    @gram.destroy
+    redirect_to root_path
+  end
+
   def update
     @gram = Gram.find_by_id(params[:id])
     return render_not_found if @gram.blank?
@@ -38,7 +45,8 @@ class GramsController < ApplicationController
     else
       render :new, status: :unprocessable_entity
     end  
-  end
+  
+end
 
   private
 
